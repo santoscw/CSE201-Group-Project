d8783dfe0ec18744e24a64b71375370b5614d200
Benjamin Arehart <benjamin@arehart.com>
2019-04-15 01:10:40 -0400

    Add Breed
    Add breed form created and functional.
    Side note: using a shortened url as an image source in an img tag still works.

--- website/index.php
+++ website/index.php
@@ -2,7 +2,7 @@
 	require_once 'phpimports/header.php';
 	
 	$nameErr = $emailErr = $commentsErr = $submitmsg = $error = $level = $username = $loggedin = null;
-	
+	$name2Err = $sectionErr = $countryErr = $imageErr = null;
 	session_start();
 	if (isset($_POST['logout'])) {
 		session_unset();
@@ -39,7 +39,42 @@
 
 
 //	$response = null;
-	if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['comments'])) 
+	if (isset($_POST['breed'])) 
+	{
+		$name_temp = mysql_sanitize_db_input_info($_POST['name2']);
+		$section_temp = mysql_sanitize_db_input_info($_POST['section']);
+		$country_temp = mysql_sanitize_db_input_info($_POST['country']);
+		$image_temp = mysql_sanitize_db_input_info($_POST['image']);
+		
+		if ($_SERVER['REQUEST_METHOD'] == "POST" && $name_temp == null)
+		{
+			$name2Err = "*Required";
+		}
+		if ($_SERVER['REQUEST_METHOD'] == "POST" && $section_temp == null)
+		{
+			$sectionErr = "*Required";
+		}
+		if ($_SERVER['REQUEST_METHOD'] == "POST" && $country_temp == null)
+		{
+			$countryErr = "*Required";
+		}
+		if ($_SERVER['REQUEST_METHOD'] == "POST" && $image_temp == null)
+		{
+			$imageErr = "*Required";
+		}
+		if ($nameErr == null && $sectionErr == null && $countryErr == null && $imageErr == null) {
+			
+			$query = "INSERT INTO dog (name, section, country, image) VALUES ('$name_temp', '$section_temp', '$country_temp', '$image_temp')";
+			$result = queryData($query);
+			
+			if (!$result) die($data->error);
+			else
+			{
+				$submitmsg = "<p class='submit'>Thanks for the comment!</p>";
+			}
+		}
+	} 
+	else if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['comments'])) 
 	{
 		$name_temp = mysql_sanitize_db_input_info($_POST['name']);
 		$email_temp = mysql_sanitize_db_input_info($_POST['email']);
@@ -175,6 +210,55 @@
 				</form>
 			</div>
 		</div>
+		<div class="container">
+			<div data-form="ui-body-a" id="breedForm" data-theme="a" class="ui-body ui-body-a ui-corner-all">
+				<form data-form="ui-body-a" id="breedForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" data-ajax="false">
+					<div class="row">
+						<div class="container">
+							<div class="twelve columns">
+								<h2 class="title">Add entry</h2>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="container">
+							<div class="twelve columns">
+								<div data-form="ui-body-a" class="ui-li-static ui-field-contain">
+									<label for="name2">Name: </label>
+									<input type="text" name="name2" data-mini="true" autofocus />
+									<span class="ui-custom-inherit"><?php echo $name2Err;?></span>
+								</div>
+								<div data-form="ui-body-a" class="ui-li-static ui-field-contain">
+									<label for="section">Section: </label>
+									<input type="text" name="section" data-mini="true" />
+									<span class="ui-custom-inherit"><?php echo $sectionErr;?></span>
+								</div>
+								<div data-form="ui-body-a" class="ui-li-static ui-field-contain">
+									<label for="country">Country: </label>
+									<input type="text" name="country" data-mini="true" />
+									<span class="ui-custom-inherit"><?php echo $countryErr;?></span>
+								</div>
+								<div data-form="ui-body-a" class="ui-li-static ui-field-contain">
+									<label for="image">Image Link: </label>
+									<input type="text" name="image" data-mini="true" />
+									<span class="ui-custom-inherit"><?php echo $imageErr;?></span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="container">
+							<div class="twelve columns">
+								<input type="hidden" name="breed" value="yes" />
+								<a class="ui-btn ui-input-btn ui-btn-icon-right ui-icon-comment ui-corner-all submitProxy" data-form="ui-btn-up-a">Submit</a>
+								
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+
 		<style>
 body, html {
     height: 100%;
