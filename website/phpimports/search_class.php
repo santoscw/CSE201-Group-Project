4991c01f36647fa349d78a52b0a1602d25018a2b
Benjamin Arehart <benjamin@arehart.com>
2019-04-18 18:39:46 -0400

    v2.2.3 Advanced Search

--- website/phpimports/search_class.php
+++ website/phpimports/search_class.php
@@ -198,114 +198,115 @@ _STRING;
         $query = $this->buildQuery();
         $result = queryData($query);
         if (!$result) {
-            die($data->error);
-        }
-        $rows = $result->num_rows;
-        if ($this->table == "dog") {
-            $outputtable = <<<_STRING
-            <table data-role="table" id="searchTable" data-mode="columntoggle" class="ui-responsive ui-table ui-corner-all">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th data-priority="4">Age</th>
-                    <th data-priority="2">Breed</th>
-                    <th data-priority="3">Shelter</th>
-                    <th data-priority="1">Look at</th>
-                </tr>
-            </thead>
-            <tbody>
-_STRING;
-            for ($j = 0; $j < $rows; ++$j) {
-                $result->data_seek($j);
-                $row = $result->fetch_array(MYSQLI_ASSOC);
-                $documentfunc = htmlspecialchars($_SERVER["PHP_SELF"]);
-                $outputrow = <<<_STRING
-                <tr>
-                <td>{$row['name']}</td>
-                <td>{$row['age']}</td>
-                <td>{$row['breed']}</td>
-                <td>{$row['shelter']}</td>
-                <td>
-                <form data-form="ui-body-a" method="post" action="dog.php" data-ajax="false">
-                <input type="hidden" value="{$row['id']}" name="entry" />
-                <a href="#" data-role="button" class="ui-btn ui-corner-all submitProxy">Look At</a>
-                </form>
-                </td>
-                </tr>
-_STRING;
-                $outputtable = $outputtable . $outputrow;
-            }
-            $outputtable = $outputtable . "</tbody></table>";
-        } else if ($this->table == "breed") {
-            $outputtable = <<<_STRING
-			<table data-role="table" id="breedTable" data-mode="columntoggle" class="ui-responsive ui-table ui-corner-all">
-				<thead>
-					<tr>
-						<th>Name</th>
-                        <th data-priority="2">Image</th>
-                        <th data-priority="3">Type</th>
-						<th data-priority="1">Look at</th>
-					</tr>
-				</thead>
-				<tbody>
-_STRING;
-            for ($j = 0; $j < $rows; ++$j) {
-                $result->data_seek($j);
-                $row = $result->fetch_array(MYSQLI_ASSOC);
-                $documentfunc = htmlspecialchars($_SERVER["PHP_SELF"]);
-                $outputrow = <<<_STRING
-                <tr>
-                <td>{$row['name']}</td>
-                <td><img src="{$row['image']}"></img></td>
-                <td>{$row['type']}</td>
-                <td>
-                <form data-form="ui-body-a" method="post" action="breed.php" data-ajax="false">
-                <input type="hidden" value="{$row['id']}" name="entry" />
-                <a href="#" data-role="button" class="ui-btn ui-corner-all submitProxy">Look At</a>
-                </form>
-                </td>
-                </tr>
-_STRING;
-                $outputtable = $outputtable . $outputrow;
-            }
-            $outputtable = $outputtable . "</tbody></table>";
-        } else if ($this->table == "shelter") {
-            $outputtable = <<<_STRING
-            <table data-role="table" id="searchTable" data-mode="columntoggle" class="ui-responsive ui-table ui-corner-all">
+            return "MySQL Query error. Will be rectified shortly.\n" . $data->error;
+        } else {
+            $rows = $result->num_rows;
+            if ($this->table == "dog") {
+                $outputtable = <<<_STRING
+                <table data-role="table" id="searchTable" data-mode="columntoggle" class="ui-responsive ui-table ui-corner-all">
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th data-priority="2">City</th>
-                        <th data-priority="3">Phone</th>
+                        <th data-priority="4">Age</th>
+                        <th data-priority="2">Breed</th>
+                        <th data-priority="3">Shelter</th>
                         <th data-priority="1">Look at</th>
                     </tr>
                 </thead>
                 <tbody>
 _STRING;
-            for ($j = 0; $j < $rows; ++$j) {
-                $result->data_seek($j);
-                $row = $result->fetch_array(MYSQLI_ASSOC);
-                $documentfunc = htmlspecialchars($_SERVER["PHP_SELF"]);
-                $outputrow = <<<_STRING
-                <tr>
+                for ($j = 0; $j < $rows; ++$j) {
+                    $result->data_seek($j);
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+                    $documentfunc = htmlspecialchars($_SERVER["PHP_SELF"]);
+                    $outputrow = <<<_STRING
+                    <tr>
                     <td>{$row['name']}</td>
-                    <td>{$row['city']}</td>
-                    <td>{$row['phone']}</td>
+                    <td>{$row['age']}</td>
+                    <td>{$row['breed']}</td>
+                    <td>{$row['shelter']}</td>
                     <td>
-                        <form data-form="ui-body-a" method="post" action="shelter.php" data-ajax="false">
-                            <input type="hidden" value="{$row['id']}" name="entry" />
-                            <a href="#" data-role="button" class="ui-btn ui-corner-all submitProxy">Look At</a>
-                        </form>
+                    <form data-form="ui-body-a" method="post" action="dog.php" data-ajax="false">
+                    <input type="hidden" value="{$row['id']}" name="entry" />
+                    <a href="#" data-role="button" class="ui-btn ui-corner-all submitProxy">Look At</a>
+                    </form>
                     </td>
-                </tr>
+                    </tr>
 _STRING;
-                $outputtable = $outputtable . $outputrow;
+                    $outputtable = $outputtable . $outputrow;
+                }
+                $outputtable = $outputtable . "</tbody></table>";
+            } else if ($this->table == "breed") {
+                $outputtable = <<<_STRING
+                <table data-role="table" id="breedTable" data-mode="columntoggle" class="ui-responsive ui-table ui-corner-all">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th data-priority="2">Image</th>
+                            <th data-priority="3">Type</th>
+                            <th data-priority="1">Look at</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+_STRING;
+                for ($j = 0; $j < $rows; ++$j) {
+                    $result->data_seek($j);
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+                    $documentfunc = htmlspecialchars($_SERVER["PHP_SELF"]);
+                    $outputrow = <<<_STRING
+                    <tr>
+                    <td>{$row['name']}</td>
+                    <td><img src="{$row['image']}"></img></td>
+                    <td>{$row['type']}</td>
+                    <td>
+                    <form data-form="ui-body-a" method="post" action="breed.php" data-ajax="false">
+                    <input type="hidden" value="{$row['id']}" name="entry" />
+                    <a href="#" data-role="button" class="ui-btn ui-corner-all submitProxy">Look At</a>
+                    </form>
+                    </td>
+                    </tr>
+_STRING;
+                    $outputtable = $outputtable . $outputrow;
+                }
+                $outputtable = $outputtable . "</tbody></table>";
+            } else if ($this->table == "shelter") {
+                $outputtable = <<<_STRING
+                <table data-role="table" id="searchTable" data-mode="columntoggle" class="ui-responsive ui-table ui-corner-all">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th data-priority="2">City</th>
+                            <th data-priority="3">Phone</th>
+                            <th data-priority="1">Look at</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+_STRING;
+                for ($j = 0; $j < $rows; ++$j) {
+                    $result->data_seek($j);
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+                    $documentfunc = htmlspecialchars($_SERVER["PHP_SELF"]);
+                    $outputrow = <<<_STRING
+                    <tr>
+                        <td>{$row['name']}</td>
+                        <td>{$row['city']}</td>
+                        <td>{$row['phone']}</td>
+                        <td>
+                            <form data-form="ui-body-a" method="post" action="shelter.php" data-ajax="false">
+                                <input type="hidden" value="{$row['id']}" name="entry" />
+                                <a href="#" data-role="button" class="ui-btn ui-corner-all submitProxy">Look At</a>
+                            </form>
+                        </td>
+                    </tr>
+_STRING;
+                    $outputtable = $outputtable . $outputrow;
+                }
+                $outputtable = $outputtable . "</tbody></table>";
             }
-            $outputtable = $outputtable . "</tbody></table>";
-        }
-        $result->close();
+            $result->close();
 
-        return $outputtable;
+            return $outputtable;
+        }
     }
 
     public function addTen() {
