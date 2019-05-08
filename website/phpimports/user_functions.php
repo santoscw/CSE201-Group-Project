1462706f903146d95b7c0b2b29b92bf74f5303ed
Benjamin <benjamin@arehart.com>
2019-05-08 15:36:59 -0400

    Added PHPDoc comments to user_functions.php

--- website/phpimports/user_functions.php
+++ website/phpimports/user_functions.php
@@ -1,6 +1,11 @@
 <?php
     require_once 'phpimports/functions.php';
     
+    /**
+     * function to update a user with admin permissions
+     * 
+     * @return string  the error message if the query failed, or success message if it didn't
+     */
     function makeAdmin($admin_target) {
         $query = "UPDATE `user` SET `level` = '2' WHERE `user`.`uid` = $admin_target";
         $result = queryData($query);
@@ -11,6 +16,11 @@
         }
     }
 
+    /**
+     * function to update a user with moderator permissions
+     * 
+     * @return string  the error message if the query failed, or success message if it didn't
+     */
     function makeMod($mod_target) {
         $query = "UPDATE `user` SET `level` = '1' WHERE `user`.`uid` = $mod_target";
         $result = queryData($query);
